30d013f7886708ac8e22f7614a3b36f5b9928279
Jose <al_jperaltaa@iessierradelasvillas.org>
2023-03-03 11:09:39 +0100

    TERMINADO FALTA EL MERGE

--- Login.py
+++ Login.py
@@ -6,39 +6,49 @@ def main(page: ft.Page):
 
     #Ejercicio 6
 
+    dlg = ft.AlertDialog(title=ft.Text("CONTRASEÑA CORRECTA!"))
+
+    def login(e):
+        if tfnombre.value==tfpassword.value:
+            page.dialog = dlg
+            dlg.open = True
+            page.update()
+
     #Fin --- Ejercicio 6
 
 
     #Ejercicio 2
     
+
+    img = ft.Image(src=f"Logo.png",width=100,height=100,fit=ft.ImageFit.CONTAIN,)
     
     #Fin --- Ejercicio 2
 
 
     #Ejercicio 3
-    
+    page.window_height=500
+    page.window_width= 250
+    page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
+    page.update()
 
     #Fin --- Ejercicio 3
 
-    page.window_height=500
-    page.window_width= 250
-
-
-
     tfnombre = ft.TextField(label="Nombre")
-    #Ejercicio 4
     
-
-    #Fin --- Ejercicio 4
-
-
     #Ejercicio 5
+
+    btnEntrar=ft.ElevatedButton("GUARDAR",icon="settings",on_click=login)
 
     #Fin-- Ejercicio 5
 
 
-    page.add(img,tfnombre,tfpassword,btnEntrar)
+    #Ejercicio 4
     
+    tfpassword=ft.TextField(label="Contraseña", password=True, can_reveal_password=True)
+
+    #Fin --- Ejercicio 4
+
+    page.add(img,tfnombre,tfpassword,btnEntrar)
 
 
-ft.app(target=main,assets_dir="imagenes")
+ft.app(target=main,assets_dir="fotos")
